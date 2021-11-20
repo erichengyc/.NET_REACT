100f06dfff87b36ea5b0fcf9f49429306669cbf6
erichengyc <68725887+erichengyc@users.noreply.github.com>
2021-11-20 21:23:47 +0930

    adding some styles to nav bar

--- Reactivities/client-app/src/app/layout/App.tsx
+++ Reactivities/client-app/src/app/layout/App.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { Container, Header, List } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from '../layout/NavBar';
 
@@ -14,16 +14,20 @@ function App() {
   }, [])
 
   return (
-    <div>
+    <>
       <NavBar />
-      <List>
+      <Container style={{marginTop: '7em'}}> 
+        <List>
         {activities.map(activity => (
-          <List.Item key={activity.id}>
+          <List.Item keyF={activity.id}>
             {activity.title}
           </List.Item>
         ))}
       </List>
-    </div>
+      </Container>
+
+
+    </>
   );
 }
 
