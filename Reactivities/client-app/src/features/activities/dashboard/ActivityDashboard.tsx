b36d7589547faf01cf2746f5be5af64026af01e6
erichengyc <68725887+erichengyc@users.noreply.github.com>
2021-11-20 21:48:30 +0930

    activity list

--- Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,22 +1,17 @@
 import React from "react";
 import { Grid, List } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
+import ActivityList from "./ActivityList";
 
 interface Props {
     activities: Activity[];
 }
 
-export default function ActivityDashboard({activities}: Props) {
+export default function ActivityDashboard({ activities }: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
-                <List>
-                    {activities.map(activity => (
-                        <List.Item keyF={activity.id}>
-                            {activity.title}
-                        </List.Item>
-                    ))}
-                </List>
+                <ActivityList activities={activities}/>
             </Grid.Column>
         </Grid>
     )
