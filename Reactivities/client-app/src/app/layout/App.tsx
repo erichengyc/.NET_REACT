ab8fc46024c3bfa5e694430c29789105db3bfa29
erichengyc <68725887+erichengyc@users.noreply.github.com>
2021-11-20 21:11:49 +0930

    nav bar

--- Reactivities/client-app/src/app/layout/App.tsx
+++ Reactivities/client-app/src/app/layout/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
+import NavBar from '../layout/NavBar';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -14,14 +15,14 @@ function App() {
 
   return (
     <div>
-      <Header as='h2' icon='users' content='Reactivities'/>
-        <List>
-          {activities.map(activity => (
-            <List.Item key={activity.id}>
-              {activity.title}
-            </List.Item>
-          ))}
-        </List>
+      <NavBar />
+      <List>
+        {activities.map(activity => (
+          <List.Item key={activity.id}>
+            {activity.title}
+          </List.Item>
+        ))}
+      </List>
     </div>
   );
 }
