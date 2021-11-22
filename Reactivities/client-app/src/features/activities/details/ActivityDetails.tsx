0d2e9bae96a0475cb5f8f0e9b92fb0daec8ef873
erichengyc <68725887+erichengyc@users.noreply.github.com>
2021-11-22 17:40:46 +0930

    Populating the detailed components

--- Reactivities/client-app/src/features/activities/details/ActivityDetails.tsx
+++ Reactivities/client-app/src/features/activities/details/ActivityDetails.tsx
@@ -25,9 +25,9 @@ export default observer(function ActivityDetails() {
     return (
         <Grid>
             <Grid.Column width={10}>
-                <ActivityDetailedHeader />
-                <ActivityDetailedInfo />
-                <ActivityDetailedChat />
+                <ActivityDetailedHeader activity={activity} />
+                <ActivityDetailedInfo activity={activity}/>
+                <ActivityDetailedChat/>
             </Grid.Column>
             <Grid.Column width={6}>
                 <ActivityDetailedSidebar />
