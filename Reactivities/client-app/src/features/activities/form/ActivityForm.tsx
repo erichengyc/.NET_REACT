ba5f6f12423ff801fcf708306dd21683c576ae8f
erichengyc <68725887+erichengyc@users.noreply.github.com>
2021-11-22 23:44:49 +0930

    Creating a reusable text area

--- Reactivities/client-app/src/features/activities/form/ActivityForm.tsx
+++ Reactivities/client-app/src/features/activities/form/ActivityForm.tsx
@@ -9,6 +9,7 @@ import { Link } from 'react-router-dom';
 import { Formik, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import MyTextInput from '../../../app/common/form/MyTextInput';
+import MyTextArea from '../../../app/common/form/MyTextArea';
 
 export default observer(function ActivityForm() {
     const history = useHistory();
@@ -66,11 +67,9 @@ export default observer(function ActivityForm() {
                 initialValues={activity}
                 onSubmit={values => console.log(values)}>
                 {({ handleSubmit }) => (
-                    <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
-                        <MyTextInput name='title' placeholder='Title'/>
-
+                    <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>   
                         <MyTextInput placeholder='Title' name='title' />
-                        <MyTextInput placeholder='Description' name='description' />
+                        <MyTextArea rows={3} placeholder='Description' name='description' />
                         <MyTextInput placeholder='Category' name='category' />
                         <MyTextInput placeholder='Date' name='date' />
                         <MyTextInput placeholder='City' name='city' />
